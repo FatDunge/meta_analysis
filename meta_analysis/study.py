86ffaddb1acfbc38cb3f92c158732e0eca264396
‘e <kangxiaopeng2018@ia.ac.cn>
2020-02-21 20:03:20 +0800

    store method

--- meta_analysis/study.py
+++ meta_analysis/study.py
@@ -5,12 +5,18 @@ import numpy as np
 @dataclass
 class Study(object):
     name: str
+    method: str
     m1: float
     s1: float
     n1: int
     m2: float
     s2: float
     n2: int
+
+    def __post_init__(self):
+        self.s = None
+        if self.method == 'cohen_d':
+            self.func = self.cohen_d
 
     def cohen_d(self):
         m1 = self.m1
@@ -19,7 +25,14 @@ class Study(object):
         m2 = self.m2
         s2 = self.s2
         n2 = self.n2
-        self.s = np.sqrt(((n1-1)*s1**2+(n2-1)*s2**2)/(n1+n2-2))
+        self.s = np.sqrt(((n1-1)*(s1**2)+(n2-1)*(s2**2))/(n1+n2-2))
         d = (m1 - m2) / s
-        self.es = d
         return d
+
+    def get_effect_size(self):
+        return self.func()
+    
+    def get_variance(self):
+        if not self.s:
+            self.func()
+        return self.s
