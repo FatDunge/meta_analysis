2d5bcf2fd389d3cfc1d53ad860ec93460a612d3b
‘e <kangxiaopeng2018@ia.ac.cn>
2020-02-21 20:02:52 +0800

    change var name, add attr to Model

--- meta_analysis/model.py
+++ meta_analysis/model.py
@@ -2,8 +2,20 @@ import numpy as np
 import scipy
 
 class Model(object):
-    def __init__(self):
+    def __init__(self, effect_sizes, variances):
         super().__init__()
+        self.effect_sizes = effect_sizes
+        self.variance = variances
+
+        self.total_effect_size = None
+        self.weights = None
+        self.total_variance = None
+        self.standard_error = None
+        self.lower_limits =None
+        self.upper_limits = None
+        self.q = None
+        self.z = None
+        self.p = None
 
     def caculate(self):
         raise NotImplementedError
@@ -16,36 +28,55 @@ def get_confidence_intervals(effect_size, standard_error):
     upper_limits = effect_size + 1.96 * standard_error
     return lower_limits, upper_limits
 
-def get_heterogeneity(effect_sizes, total_es, weights):
-    diff_es_square = (effect_sizes - total_es) ** 2
+def get_heterogeneity(effect_sizes, total_effect_size, weights):
+    diff_es_square = (effect_sizes - total_effect_size) ** 2
     q = np.sum(np.multiply(weights, diff_es_square))
     return q
 
-def get_z_value(total_es, standard_error, x0=0):
-    return (total_es - x0) / standard_error
+def get_z_value(total_effect_size, standard_error, x0=0):
+    return (total_effect_size - x0) / standard_error
 
 def get_p_from_z(z, one_side=False):
     if one_side:
         p_values = scipy.stats.norm.sf(abs(z))
     else:
-        p_values = scipy.stats.norm.sf(abs(z))*2
+        p_values = scipy.stats.norm.sf(abs(z)) * 2
 
 class FixedModel(Model):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, effect_sizes, variances):
+        super().__init__(effect_sizes, variances)
 
-    def caculate(self, effect_sizes, variances):
+        self.caculate()
+
+    def caculate(self):
+        variances = self.variances
+        effect_sizes = self.effect_sizes
         weights = np.reciprocal(variances)
-        total_es = np.sum(np.multiply(effect_sizes, weights)) /\
+        total_effect_size = np.sum(np.multiply(effect_sizes, weights)) /\
                    np.sum(weights)
-        total_v = 1 / np.sum(weights)
-        standard_error = np.sqrt(total_v)
+        total_variance = 1 / np.sum(weights)
+        standard_error = np.sqrt(total_variance)
 
-        ll, ul = get_confidence_intervals(total_es, standard_error)
-        q = get_heterogeneity(effect_sizes, total_es, weights)
-        z = get_z_value(total_es, standard_error)
+        lower_limits, upper_limits = get_confidence_intervals(total_effect_size, standard_error)
+        q = get_heterogeneity(effect_sizes, total_effect_size, weights)
+        z = get_z_value(total_effect_size, standard_error)
         p = get_p_from_z(z)
 
+        self.total_effect_size = total_effect_size
+        self.weights = weights
+        self.total_variance = total_variance
+        self.standard_error = standard_error
+        self.lower_limits = lower_limits
+        self.upper_limits = upper_limits
+        self.q = q
+        self.z = z
+        self.p = p
+
+    def get_results(self):
+        return (self.total_effect_size, self.weights, 
+                self.total_variance, self.standard_error,
+                self.lower_limits, self.upper_limits,
+                self.q, self.z, self.p)
 
 
     
