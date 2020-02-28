b8be65dfd03b23a627bff55076dd0188a03ddc33
‘e <kangxiaopeng2018@ia.ac.cn>
2020-02-28 21:38:51 +0800

    combine data.py and study.py, add group class.

--- meta_analysis/data.py
+++ meta_analysis/data.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
+from typing import Any
+
 import nibabel as nib
 import numpy as np
-from .study import Study
 
 def load_array(path):
     nii = nib.load(path)
@@ -14,26 +16,77 @@ def load_arrays(pathes):
         arrays = np.stack([load_array(path) for path in pathes], axis=0)
     return arrays
 
-def get_mean_std_n(arrays):
+def cal_mean_std_n(arrays):
     mean = np.mean(arrays, axis=0)
     std = np.std(arrays, axis=0)
     n = arrays.shape[0]
     return mean, std, n
 
+@dataclass
+class Group(object):
+    mean: Any
+    std: Any
+    count: int
+    label: Any
+
+    def __post_init__(self):
+        self.mean = np.asarray(self.mean)
+        self.std = np.asarray(self.std)
+
+    def get_label(self):
+        return self.label
+    
+    def get_mean_std_count(self, index):
+        if index:
+            return self.mean[index], self.std[index], self.count
+        else:
+            return self.mean.item(0), self.std.item(0), self.count
+
+@dataclass
+class Study(object):
+    name: str
+    method: str
+    group_experimental: Group
+    group_control: Group
+
+    def __post_init__(self):
+        self.s = None
+        if self.method == 'cohen_d':
+            self.func = self.cohen_d
+
+    def cohen_d(self, index):
+        m1, s1, n1 = self.group_experimental.get_mean_std_count(index)
+        m2, s2, n2 = self.group_control.get_mean_std_count(index)
+        self.s = np.sqrt(((n1-1)*(s1**2)+(n2-1)*(s2**2))/(n1+n2-2))
+        d = (m1 - m2) / s
+        return d
+
+    def get_effect_size(self, index=None):
+        return self.func(index)
+    
+    def get_variance(self):
+        if not self.s:
+            self.func()
+        return self.s
+
 class Center(object):
-    def __init__(self, name, filepathes1, filepathes2):
+    def __init__(self, name, groups):
         self.name = name
-        arrays1 = load_arrays(filepathes1)
-        arrays2 = load_arrays(filepathes2)
-        self.array_shape = arrays1[0].shape
+        self.build_dic(groups)
 
-        self.m1, self.s1, self.n1 = get_mean_std_n(arrays1)
-        self.m2, self.s2, self.n2 = get_mean_std_n(arrays2)
+    def build_dic(self, groups):
+        group_dict = {}
+        for group in groups:
+            label = group.get_label()
+            if label not in group_dict:
+                group_dict[label] = group
+            else:
+                raise ValueError("Two Groups have same label, please check or merge.")
+        self.group_dict = group_dict
 
-    def gen_study(self, index, method):
-        if self.n1 and self.n2:
+    def gen_study(self, label1, label2, method):
+        if label1 in self.group_dict and label2 in self.group_dict:
             return Study(self.name, method,
-                         self.m1[index], self.s1[index], self.n1[index],
-                         self.m2[index], self.s2[index], self.n2[index])
-    
-    
+                         self.group_dict[label1],
+                         self.group_dict[label2])
+
