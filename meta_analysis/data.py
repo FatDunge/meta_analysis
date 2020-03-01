b86a038a64384da43f9a5b62c3522f63c1e65b9b
‘e <kangxiaopeng2018@ia.ac.cn>
2020-03-01 20:17:36 +0800

    using index. check array shape

--- meta_analysis/data.py
+++ meta_analysis/data.py
@@ -32,6 +32,7 @@ class Group(object):
     def __post_init__(self):
         self.mean = np.asarray(self.mean)
         self.std = np.asarray(self.std)
+        assert self.mean.shape == self.std.shape
 
     def get_label(self):
         return self.label
@@ -57,26 +58,35 @@ class Study(object):
     def cohen_d(self, index):
         m1, s1, n1 = self.group_experimental.get_mean_std_count(index)
         m2, s2, n2 = self.group_control.get_mean_std_count(index)
-        self.s = np.sqrt(((n1-1)*(s1**2)+(n2-1)*(s2**2))/(n1+n2-2))
+        s = np.sqrt(((n1-1)*(s1**2)+(n2-1)*(s2**2))/(n1+n2-2))
         d = (m1 - m2) / s
+        self.s = s
         return d
 
     def get_effect_size(self, index=None):
         return self.func(index)
     
-    def get_variance(self):
+    def get_variance(self, index=None):
         if not self.s:
-            self.func()
+            self.func(index)
         return self.s
 
 class Center(object):
     def __init__(self, name, groups):
         self.name = name
-        self.build_dic(groups)
+        self.groups = groups
+        self.check_groups()
+        self.build_dic()
 
-    def build_dic(self, groups):
+    def check_groups(self):
+        shape = self.groups[0].mean.shape
+        for group in self.groups:
+            assert group.mean.shape == shape
+        self.shape = shape
+
+    def build_dic(self):
         group_dict = {}
-        for group in groups:
+        for group in self.groups:
             label = group.get_label()
             if label not in group_dict:
                 group_dict[label] = group
